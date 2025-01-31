7edff27dc4a43547bbb4f225c9fa19788ae2f3bb
zewexqa <domtui555@gmail.com>
2025-01-31 13:37:25 +0700

    lab15_1.cpp Finished

--- lab15_1.cpp
+++ lab15_1.cpp
@@ -1,9 +1,29 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-
-	
-	
-	return 0;
+int main() {
+    int a = 5;
+    char b = 'A';
+    char &c = b;
+    int *x = &a;
+    char *y = &b;
+    int **z = &x;
+    
+    cout << a << " " << b << " " << c << " " << x << " " << (void*)y << " " << z << endl;
+    
+    cout << &a << " " << (void*)&b << " " << (void*)&c << " " << &x << " " << &y << " " << &z << endl;
+    
+    c = 'F';
+    cout << a << " " << b << " " << c << " " << x << " " << (void*)y << " " << z << endl;
+    
+    *y = 'W';
+    cout << a << " " << b << " " << c << " " << x << " " << (void*)y << " " << z << endl;
+    
+    *x = 6;
+    cout << a << " " << b << " " << c << " " << x << " " << (void*)y << " " << z << endl;
+    
+    **z = 7;
+    cout << a << " " << b << " " << c << " " << x << " " << (void*)y << " " << z << endl;
+    
+    return 0;
 }
